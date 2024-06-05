04a76b0d504b5fca4beda87d739026d4fb59539f
Sean Outram <outramsean0@gmail.com>
2024-06-05 09:32:55 +0100

    Switch to breadboardOS 2nd attempt

---
+++ project.cmake
@@ -0,0 +1,14 @@
+# PROJECT NAME - in quotes, no spaces
+set(PROJ_NAME "hack-hour-clock")
+
+# PROJECT VERSION - in quotes, no spaces, can contain alphanumeric if necessary
+set(PROJ_VER "0.0")
+
+# CLI INTERFACE - 0: use UART for CLI (default), 1: use USB for CLI
+set(CLI_IFACE 1)
+
+# MCU PLATFORM - set the MCU platform being used (i.e. the subdir in 'hardware/')
+set(PLATFORM rp2040)
+
+# BOARD - set the board being used (i.e. 'pico', 'pico_w', etc)
+set(BOARD pico_w)
