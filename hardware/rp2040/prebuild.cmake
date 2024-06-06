b0866eed8bee396642eb2a753e14ea4a0e5fad8b
Sean Outram <outramsean0@gmail.com>
2024-06-06 17:21:00 +0000

    I really need to check my changes first...

--- hardware/rp2040/prebuild.cmake
+++
@@ -1,7 +0,0 @@
-set(PICO_BOARD ${BOARD})
-
-# Raspberry Pi Pico (RP2040) SDK
-include(${hardware_dir}/pico_sdk_import.cmake)
-
-set(hardware_includes ${hardware_dir})
-set(hardware_libs "pico_unique_id" "pico_stdlib")
--- utils.h
+++
@@ -1,3 +0,0 @@
-#define UBYTE   uint8_t
-#define UWORD   uint16_t
-#define UDOUBLE uint32_t
