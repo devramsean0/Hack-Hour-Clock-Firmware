1a11f38250126a7202003ee0d1a8d612f34493ea
Sean Outram <outramsean0@gmail.com>
2024-06-07 16:50:09 +0100

    More progress

--- Hack-Hour-Clock-Firmware.c
+++ Hack-Hour-Clock-Firmware.c
@@ -12,9 +12,34 @@
 // We are going to use SPI 0, and allocate it to the following GPIO pins
 // Pins can be changed, see the GPIO function select table in the datasheet for information on GPIO assignments
 #define SPI_PORT spi1
-#define RCLK_PIN 8
+#define RCLK_PIN 9
 #define CLK_PIN 10
 #define MOSI_PIN 11
+
+UBYTE SEG8Code[] ={
+        0x3F, // 0
+        0x06, // 1
+        0x5B, // 2
+        0x4F, // 3
+        0x66, // 4
+        0x6D, // 5
+        0x7D, // 6
+        0x07, // 7
+        0x7F, // 8
+        0x6F, // 9
+        0x77, // A
+        0x7C, // b
+        0x39, // C
+        0x5E, // d
+        0x79, // E
+        0x71, // F
+    };
+
+#define KILOBIT   0XFE //KiloBit
+#define HUNDREDS  0XFD //hundreds
+#define TENS      0XFB //tens
+#define UNITS     0XF7 //units
+#define Dot       0x80
 
 void GPIO_MODE(UWORD Pin, UWORD Mode) {
     gpio_init(Pin);
@@ -25,22 +50,6 @@ void GPIO_MODE(UWORD Pin, UWORD Mode) {
     }
 }
 
-int init_8seg_led() {
-    spi_init(SPI_PORT, 1000*1000);
-
-    gpio_set_function(CLK_PIN, GPIO_FUNC_SPI);
-    gpio_set_function(MOSI_PIN, GPIO_FUNC_SPI);
-
-    GPIO_MODE(RCLK_PIN, 1);
-    gpio_put(RCLK_PIN, 1);
-
-    if (spi_get_hw(SPI_PORT) == NULL) {
-        return 0;
-    }
-    pico_8seg_led_send_command(0X57, 0x35);
-    return 1;
-};
-
 void led_send_command(UBYTE const Num, UBYTE const Seg) {
     gpio_put(RCLK_PIN, 1);
     spi_write_blocking(SPI_PORT, &Num, 1);
@@ -50,24 +59,40 @@ void led_send_command(UBYTE const Num, UBYTE const Seg) {
     gpio_put(RCLK_PIN, 1);
 }
 
+int init_8seg_led() {
+    spi_init(SPI_PORT, 1000*1000);
+
+    gpio_set_function(CLK_PIN, GPIO_FUNC_SPI);
+    gpio_set_function(MOSI_PIN, GPIO_FUNC_SPI);
+
+    GPIO_MODE(RCLK_PIN, 1);
+    gpio_put(RCLK_PIN, 1);
+    return 1;
+};
+
 int main()
 {
     stdio_init_all();
 
     // Initialise the Wi-Fi chip
-    if (cyw43_arch_init()) {
-        printf("Wi-Fi init failed\n");
-        return -1;
+    printf("Connecting to Wi-Fi...\n");
+    if (cyw43_arch_wifi_connect_timeout_ms(WIFI_SSID, WIFI_PASS, CYW43_AUTH_WPA2_AES_PSK, 30000)) {
+        printf("failed to connect.\n");
+        return 1;
+    } else {
+        printf("Connected.\n");
     }
 
-    if (init_8seg_led) {
+    if (init_8seg_led()) {
         printf("8-segment LED init success");
+    } else {
+        printf("8-segment LED init failed");
     }
 
     // Enable wifi station
     cyw43_arch_enable_sta_mode();
 
-    printf("Connecting to Wi-Fi...\n");
+    /* printf("Connecting to Wi-Fi...\n");
     if (cyw43_arch_wifi_connect_timeout_ms("Your Wi-Fi SSID", "Your Wi-Fi Password", CYW43_AUTH_WPA2_AES_PSK, 30000)) {
         printf("failed to connect.\n");
         return 1;
@@ -76,10 +101,9 @@ int main()
         // Read the ip address in a human readable way
         uint8_t *ip_address = (uint8_t*)&(cyw43_state.netif[0].ip_addr.addr);
         printf("IP address %d.%d.%d.%d\n", ip_address[0], ip_address[1], ip_address[2], ip_address[3]);
-    }
+    } */
 
     while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
+        led_send_command(0X57, SEG8Code[0]);
     }
 }
